009451031b1167cc546776dc3d75b31c5e0b50af
Julio <rodrigo_l321@hotmail.com>
2020-12-05 18:15:44 -0600

    paginacion

--- src/main/java/com/solucionexpress/microservicios/app/cursos/models/repository/CursoRepository.java
+++ src/main/java/com/solucionexpress/microservicios/app/cursos/models/repository/CursoRepository.java
@@ -3,9 +3,9 @@ package com.solucionexpress.microservicios.app.cursos.models.repository;
 import com.solucionexpress.microservicios.app.cursos.models.entity.Curso;
 
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface CursoRepository extends CrudRepository <Curso,Long>{
+public interface CursoRepository extends PagingAndSortingRepository<Curso,Long> {
 	
 	@Query("select c from Curso c join fetch c.alumnos a where a.id=?1")
 	public Curso findCursoByAlumnoId(Long id);
